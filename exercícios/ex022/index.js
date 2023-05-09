7d70278851d99e5df82ff6ec4b24fa930ff98237
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-05-09 12:07:35 -0300

    Novos exercícios de objetos em JS.

---
+++ exercícios/ex022/index.js
@@ -0,0 +1,15 @@
+const circle = {
+    radius: 1,
+    draw() {
+        console.log('draw');
+    }
+};
+
+for (let key in circle) 
+    console.log(key, circle[key]); //irá mostrar chave e valor
+
+for (let key of Object.keys(circle))
+    console.log(key); //irá transformar em dois arrays com apenas as chaves
+
+for (let entry of Object.entries(circle))
+    console.log(entry); //irá transformar em dois arrays com tamanho 2 (cada um) contendo chave e valor
